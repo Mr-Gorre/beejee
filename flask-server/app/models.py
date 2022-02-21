bc61ffaa489093f40810dc665334a1fd9ce25051
Igor Potashev <fil_igor@bk.ru>
2022-02-21 14:29:48 +0300

    Create models

---
+++ flask-server/app/models.py
@@ -0,0 +1,17 @@
+from app import db
+
+
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000), unique=True)
+
+
+class Task(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    text = db.Column(db.String(1000))
+    status = db.Column(db.String(100))

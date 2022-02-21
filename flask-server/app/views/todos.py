3779ece75b24b6647b4c015ea0f9cf23bb229281
Igor Potashev <fil_igor@bk.ru>
2022-02-21 16:57:42 +0300

    Add todo create

--- flask-server/app/views/todos.py
+++ flask-server/app/views/todos.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, redirect, render_template, request
 from app import db
 from app.forms import TodoForm
+from app.models import Todo
 
 todos = Blueprint('todos', __name__)
 
@@ -9,9 +10,29 @@ todos = Blueprint('todos', __name__)
 @todos.route('/todos/')
 def list():
     form = TodoForm()
-    return render_template("todos/list.html", form=form)
+    todos = Todo.query.limit(5).all()
+    return render_template('todos/list.html', form=form, todos=todos)
+
+
+@todos.route('/todos/', methods=['POST'])
+def create():
+    form = TodoForm(request.form)
+    if form.validate_on_submit():
+        username = request.form['username']
+        email = request.form['email']
+        text = request.form['text']
+        todo = Todo(username=username, email=email, text=text)
+        db.session.add(todo)
+        db.session.commit()
+        return redirect('/todos/')
+    return render_template('todos/list.html', form=form)
 
 
 @todos.route('/todos/[id]')
 def detail():
-    return render_template("todos/detail.html")
+    return render_template('todos/detail.html')
+
+
+@todos.route('/todos/[id]/edit', methods=['POST', 'GET'])
+def edit():
+    return render_template('todos/detail.html')

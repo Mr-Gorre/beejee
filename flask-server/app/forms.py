60ea7cbfd62fd8c62feb8a4b2c8a351bb7f39039
Igor Potashev <fil_igor@bk.ru>
2022-02-22 15:16:36 +0300

    redo form to Flask Bootstrap

--- flask-server/app/forms.py
+++ flask-server/app/forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, EmailField
-from wtforms.validators import DataRequired
+from wtforms import StringField, EmailField, SubmitField
+from wtforms.validators import DataRequired, Email
 
 
 class TodoForm(FlaskForm):
     username = StringField("Username", validators=[DataRequired()])
-    email = EmailField("Email", validators=[DataRequired()])
+    email = EmailField("Email", validators=[DataRequired(), Email()])
     text = StringField("Text", validators=[DataRequired()])
+    submit = SubmitField("Submit")

f08e3fec7465d2b3d1b0adb9b2d6ce9630ecb690
Igor Potashev <fil_igor@bk.ru>
2022-02-22 10:45:56 +0300

    order by and pagination scratch

--- flask-server/app/views/todos.py
+++ flask-server/app/views/todos.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, redirect, render_template, request
+from sqlalchemy import asc, desc
 from app import db
 from app.forms import TodoForm
 from app.models import Todo
+from app.config import PAGE_SIZE
 
 todos = Blueprint('todos', __name__)
 
@@ -9,9 +11,24 @@ todos = Blueprint('todos', __name__)
 @todos.route('/')
 @todos.route('/todos/')
 def list():
+    order = request.args.get('order_by')
+    order_desc = request.args.get('desc', default=False, type= lambda x: x.lower() == "true")
+    page = request.args.get('page', type=int, default=1)
+    #TODO: hmmm
+    orders = {
+        'id': Todo.id,
+        'username': Todo.username,
+        'email': Todo.email,
+        'text': Todo.text,
+        'status': Todo.status
+    }
+    ordered_column = orders[order] if order in orders else orders['id']
+    direction = desc if order_desc else asc
+
     form = TodoForm()
-    todos = Todo.query.limit(5).all()
-    return render_template('todos/list.html', form=form, todos=todos)
+    todos = Todo.query.order_by(
+        direction(ordered_column)).paginate(page, PAGE_SIZE, False)
+    return render_template('todos/list.html', form=form, todos=todos, order_by=order, desc=order_desc)
 
 
 @todos.route('/todos/', methods=['POST'])
@@ -28,11 +45,11 @@ def create():
     return render_template('todos/list.html', form=form)
 
 
-@todos.route('/todos/[id]')
-def detail():
+@todos.route('/todos/<id>')
+def detail(id):
     return render_template('todos/detail.html')
 
 
-@todos.route('/todos/[id]/edit', methods=['POST', 'GET'])
-def edit():
+@todos.route('/todos/<id>/edit', methods=['POST', 'GET'])
+def edit(id):
     return render_template('todos/detail.html')

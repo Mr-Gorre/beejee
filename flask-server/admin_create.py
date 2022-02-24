842016b52415a5b8710f1b6e0f564a4ba5f32788
Igor Potashev <fil_igor@bk.ru>
2022-02-24 15:42:08 +0300

    Add init scripts

---
+++ flask-server/admin_create.py
@@ -0,0 +1,16 @@
+from app.models import User
+from app import db, create_app
+from werkzeug.security import generate_password_hash
+
+def main():
+    app = create_app()
+    app.app_context().push()
+    admin = User.query.filter_by(username='Admin').first()
+    if admin is None:
+        admin = User(username="Admin", password=generate_password_hash("123"))
+        db.session.add(admin)
+        db.session.commit()
+
+
+if __name__ == "__main__":
+    main()
